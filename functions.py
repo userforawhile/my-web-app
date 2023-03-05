21564921e57b736fe9ebef5b7436b3a96cbff329
Antonio Marino <marino.ant@yahoo.com>
2023-03-05 19:00:43 +0100

    initial commit

---
+++ functions.py
@@ -0,0 +1,16 @@
+FILEPATH = 'C:/Users\LATITUDE E5480/Desktop/pythonProject/pythonProject/web_app1/todo.data.txt'
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open('C:/Users\LATITUDE E5480/Desktop/pythonProject/pythonProject/web_app1/todo.data.txt', 'w') as file:
+        file.writelines(todos_arg)
